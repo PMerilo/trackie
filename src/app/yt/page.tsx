8f5a5fd3c2f424a0c9c99ecc21362112d16340b0
YewTeckNg <ngyewteck@gmail.com>
2024-01-18 22:01:35 +0800

    Webcam

--- src/app/yt/page.tsx
+++ src/app/yt/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from 'next/image'
+import Webcam from "react-webcam";
 
 export default function Home() {
   return (
@@ -6,7 +9,8 @@ export default function Home() {
       <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className="">
-            <h1 className="text-5xl font-bold">YewTeck's Page</h1>
+            <Webcam></Webcam>
+            <h1 className="text-5xl font-bold">Yew Teck's Page</h1>
             <div className="py-6 grid gap-4">
               <button className="btn btn-primary btn-outline">Login</button>
               <button className="btn btn-secondary btn-outline">Sign Up</button>
